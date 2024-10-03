1fe10fc48f115d935a94ea64646f00a974226327
William Veith <williamveith@gmail.com>
2024-10-03 07:10:45 -0500

    Updated the UI with css, title, and favicon

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def upload_list():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
