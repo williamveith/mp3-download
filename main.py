1b2d49e07c00cf7854814830290e62dddb895d5f
William Veith <williamveith@gmail.com>
2024-10-06 06:07:49 -0500

    took flask out of debug mode

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def display_log():
     return Response(generate(), mimetype='text/html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
